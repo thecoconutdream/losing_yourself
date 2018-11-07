f55516872b88b39402cf4a8f8115502291f124ad
Lisa Philine <lphiline@web.de>
2018-11-07 11:12:00 +0100

    First commit

---
+++ Assets/playerGrab.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerGrab : MonoBehaviour {
+
+    public GameObject hand;
+
+    public GameObject myHand;
+
+    bool inhands = false;
+
+    Vector3 handPosition;
+
+
+	// Use this for initialization
+	void Start () {
+        handPosition = hand.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1"))
+           {
+            if (!inhands)
+            {
+                hand.transform.SetParent(myHand.transform);
+                hand.transform.localPosition = myHand.transform.localPosition;
+                inhands = true;
+            }
+            else if(inhands)
+            {
+                hand.transform.SetParent(null);
+                hand.transform.localPosition = handPosition;
+                inhands = false;
+            }
+            }
+	}
+}
---
+++ Assets/playerWalk.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerWalk : MonoBehaviour {
+
+    public int playerSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetButton("Fire1"))
+        {
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+        }
+
+	}
+}
