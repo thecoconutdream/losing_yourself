95e20a8bdd95f90c1ba566267749787f1e2911a8
Lisa Philine <lphiline@web.de>
2018-12-05 16:48:21 +0100

    Connect Scenes

---
+++ Assets/loadingScenes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadingScenes : MonoBehaviour
+{
+
+    public string scene;
+
+    public string collidingObject;
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.name == collidingObject)
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+
+}
