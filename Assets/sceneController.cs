25a59b94b26d6fa504ea174c04f1f9cd2f16de2c
Lisa Philine <lphiline@web.de>
2018-12-19 16:37:40 +0100

    Add titlescreen and test first apk file

---
+++ Assets/sceneController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneController : MonoBehaviour {
+
+	public void ChangeScene (string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitApp()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
