b1b5644ed837f45e895698502aefd7b998dfc4c4
Lisa Philine <lphiline@web.de>
2019-01-09 17:39:03 +0100

    Add textures and ending mode

---
+++ Assets/gameover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading;
+
+public class gameover : MonoBehaviour {
+
+    public string collidingObject;
+    public GameObject ending;
+
+    // Update is called once per frame
+    void GameoverMagic ()
+    {
+        ending.SetActive(true);
+        Thread.Sleep(9000);
+        SceneManager.LoadScene("TitleScreen");
+
+    }
+   
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.name == collidingObject)
+        {
+            GameoverMagic();
+        }
+    }
+
+}
